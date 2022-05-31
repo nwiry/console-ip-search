f900d3e1b407279b1abdd68ab687d5eaaf495829
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-05-31 15:41:05 -0300

    Added functionality to fetch IPS from domain

--- app/app.go
+++ app/app.go
@@ -1,12 +1,48 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Gerar() *cli.App {
 
 	app := cli.NewApp()
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IPs e Nomes de servidor na internet!"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPs de endereços na Internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "github.com",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
 	return app
+
+}
+
+func buscarIps(c *cli.Context) {
 	
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+
 }
