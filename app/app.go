a4be845b012e33766906c2787bf6b5748aabe7f9
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-05-31 15:54:09 -0300

    Added functionality to fetch Domain NameServers
    The flags variable was created to store the content that would be repeated, as well as explanatory texts before displaying the action of the functions

--- app/app.go
+++ app/app.go
@@ -14,17 +14,25 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IPs e Nomes de servidor na internet!"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "github.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPs de endereços na Internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "github.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca IPs de endereços na Internet",
+			Flags:  flags,
 			Action: buscarIps,
+		},
+		{
+			Name:   "nameservers",
+			Usage:  "Busca Nomes de Servidor na Internet",
+			Flags:  flags,
+			Action: buscarNameServers,
 		},
 	}
 
@@ -33,7 +41,7 @@ func Gerar() *cli.App {
 }
 
 func buscarIps(c *cli.Context) {
-	
+
 	host := c.String("host")
 
 	ips, erro := net.LookupIP(host)
@@ -41,8 +49,27 @@ func buscarIps(c *cli.Context) {
 		log.Fatal(erro)
 	}
 
+	fmt.Printf("IPs do dominio %s:\n\n", host)
+
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+
+}
+
+func buscarNameServers(c *cli.Context) {
+
+	host := c.String("host")
+
+	nms, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Printf("NameServers do dominio %s:\n\n", host)
+
+	for _, ns := range nms {
+		fmt.Println(ns.Host)
 	}
 
 }
